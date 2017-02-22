6154c2896963aa5916e2e198b4d8d0274a2a0082
Manugomvil <manugom10@gmail.com>
2017-02-22 03:04:23 -0400

    Suma y Resta

---
+++ Ejercicio Suma y Resta.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+main( ) 
+{
+	int a,b,Suma,Resta,i;
+	printf("Ingreser el valor de A\n");
+	scanf("%d",&a);
+	printf("Ingreser el valor de B\n");
+	scanf("%d",&b);
+	Suma=a+b;
+	Resta= a-b;
+	printf ("El resultado es: %d y %d",Suma,Resta);
+	
+	system("pause>null");
+	
+}
